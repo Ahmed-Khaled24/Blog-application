a9dc16f6447118ce69926e3ccc955f6d48e5aa7b
Ahmed Khaled <Ahmedkhaled.m24@gmail.com>
2022-08-18 10:58:32 +0200

    feat(models): add users schema

---
+++ models/users/users.mongo.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    userSource: {
+        type: String,
+        required: true,
+    },
+    registerDate: {
+        type: Date,
+        required: true,
+    },
+    active: {
+        type: Boolean,
+        required: true,
+    }
+});
+
+const Users = mongoose.model('User', userSchema);
+
+module.exports = Users;
