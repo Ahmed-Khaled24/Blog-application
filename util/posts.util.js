41f8a894209ba68ad69a460745f51fd4d2d53918
Ahmed Khaled <Ahmedkhaled.m24@gmail.com>
2022-08-20 22:54:37 +0200

    refactor(util): change names in posts util

--- util/posts.util.js
+++ util/posts.util.js
@@ -1,29 +1,29 @@
 function validatePost(post){
     if(!post.title){ // there is no title sent from the user
         return {
-            valid : false,
-            message : 'Post title is missing'
+            isValid : false,
+            validationMessage : 'Post title is missing'
         } 
     } else if(!post.body){ // there is no body sent from the user
         return {
-            valid : false,
-            message : 'Post body is missing'
+            isValid : false,
+            validationMessage : 'Post body is missing'
         } 
     } else if(post.title.trim().length === 0){ // the user sent empty title
         return {
-            valid : false,
-            message : 'Post title is empty'
+            isValid : false,
+            validationMessage : 'Post title is empty'
         }
     } else if(post.body.trim().length === 0){ // the use sent empty body
         return {
-            valid : false,
-            message : 'Post body is empty'
+            isValid : false,
+            validationMessage : 'Post body is empty'
         }
     }
 
     return {
         valid : true,
-        message : 'Valid post'
+        validationMessage : 'Valid post'
     }
 }
 
