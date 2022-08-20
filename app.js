113f3325fc745a02e30b9146de4f9ffca2510aae
Ahmed Khaled <Ahmedkhaled.m24@gmail.com>
2022-08-20 10:41:34 +0200

    feat: update app
    add and configure cookie-session middleware,
    add passport.initialize middleware and passport.session middleware

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const express = require('express');
+const passport = require('passport');
+const session = require('cookie-session');
 const usersRouter = require('./routers/users.router');
 const postsRouter = require('./routers/posts.router');
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
-
-
+app.use(session({
+    name: 'KH-Blog',
+    keys: [process.env.COOKIE_KEY],
+    cookie:{
+        maxAge: 1000 * 60 * 60 * 24,
+    }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use('/users', usersRouter);
 app.use('/posts', postsRouter);
-
 
 module.exports = app;
