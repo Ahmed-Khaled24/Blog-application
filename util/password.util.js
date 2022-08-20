f2c6ba0ed1f21f43217756582fcd18d6cae95fa2
Ahmed Khaled <Ahmedkhaled.m24@gmail.com>
2022-08-20 10:38:36 +0200

    feat(util): add password util

---
+++ util/password.util.js
@@ -0,0 +1,24 @@
+const crypto = require('crypto');
+
+function hash(plainTextPassword, salt){
+    return hash = crypto.pbkdf2Sync(plainTextPassword, salt, 1000000, 64, 'sha512').toString('hex');
+}
+
+function generatePassword(plainTextPassword){
+    const salt = crypto.randomBytes(32).toString('hex');
+    const hashPassword = hash(plainTextPassword, salt);
+    return{
+        hashPassword,
+        salt,
+    }
+}
+
+function verifyPassword(plainTextPassword, salt, hashPassword){
+    return hashPassword === generateHash(plainTextPassword, salt);   
+}
+
+module.exports = {
+    generatePassword,
+    verifyPassword,
+}
+
