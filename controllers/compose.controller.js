c7d42ae9bb9e278618eb5a735b267094e7690c62
Ahmed Khaled <Ahmedkhaled.m24@gmail.com>
2022-08-28 20:43:33 +0200

    feat(controllers): add compose controller

---
+++ controllers/compose.controller.js
@@ -0,0 +1,36 @@
+const {validatePost} = require('../util/posts.util');
+const {db_addNewPost} = require('../models/posts/posts.model');
+
+
+function renderComposePage(req, res){
+        return res.status(200).render('compose')
+ }
+
+ 
+async function addNewPost (req, res){
+    const post  = req.body;
+    const user = req.user;
+    const { isValid, validationMessage } = validatePost(post);   
+    if(isValid){
+        post.createdBy = user.id;
+        post.createdAt = Date();
+        try {
+            await db_addNewPost(post);
+            return res.status(201).redirect('/all-posts');
+        } catch(err){
+            return res.status(500).render('compose')
+        }
+    } else {
+        return res.status(400).json({
+            error: validationMessage,
+        });
+    }
+}
+
+module.exports = {
+    renderComposePage,
+    addNewPost,
+};
+
+
+
