265376875dfb0394dc0822ea176ff18ec0b0e516
Ahmed Khaled <Ahmedkhaled.m24@gmail.com>
2022-08-30 21:04:38 +0200

    feat(util): change simplifyDate function
    change simplifyDate function to return the time passed since the post
    published

--- util/posts.util.js
+++ util/posts.util.js
@@ -27,15 +27,47 @@ function validatePost(post){
 }
 
 
-function simplifyDate(postDate){
-    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    const day = postDate.getDate();
-    const month = months[postDate.getMonth()];
-    const year = postDate.getFullYear();
-    const hour = postDate.getHours();
-    const minute = postDate.getMinutes();
-    const second = postDate.getSeconds();
-    return `${hour}:${minute}:${second} -- ${day}, ${month} ${year}`;
+function simplifyDate(postDate) {
+	const monthsName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+	const createdAt = postDate.valueOf();
+	const renderAt = Date.now();
+
+	const minutes = Math.floor((renderAt - createdAt) / (1000 * 60));
+	if (minutes < 1) {
+		return 'Now';
+	}
+	else if (minutes == 1) {
+		return `${minutes} minute ago`;
+	}
+
+	const hours = Math.floor(minutes / 60);
+	if (hours < 1) {
+		return `${minutes} minutes ago`;
+	}
+	else if (hours == 1) {
+		return `${hours} hour ago`;
+	}
+	else if (hours > 1 && hours < 24) {
+		return `${hours} hours ago`;
+	}
+
+	const days = Math.floor(hours / 24);
+	if (days == 1) {
+		return `${days} day ago`;
+	}
+	else if (days < 30) {
+		return `${days} days ago`;
+	}
+
+	const months = Math.floor(days / 30);
+	if (months == 1) {
+		return `${months} month ago`
+	}
+	else if (months < 12) {
+		return `${months} months ago`
+	}
+
+	return `${monthsName[postDate.getMonth()]}, ${postDate.getFullYear()}`
 }
 
 module.exports = {
